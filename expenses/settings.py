cbbb3e6d7531b819ab07fed8c4d4bfff98c2651a
Dziugas <dziugas.tornau@gmail.com>
2019-01-29 20:06:05 +0200

    switched to postgres, fixed charts if no data

--- expenses/settings.py
+++ expenses/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'expenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense_keeper',
+        'USER': 'postgres',
+        'PASSWORD': 'test2991',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- islaidos_app/views.py
+++ islaidos_app/views.py
@@ -3,29 +3,33 @@ from .models import Expenses, ExpenseTypes, Keeper
 from .forms import ExpenseForm, ExpenseTypeForm, KeeperForm
 from django.db.models import Sum
 
-
 def expenditure_by_date_for_google_chart(keeper_id):
     all_expenses_for_current_keeper = Expenses.objects.filter(keeper=keeper_id).order_by('data')
     distinct_expense_dates_for_that_keeper = all_expenses_for_current_keeper.values('data').distinct()
     distinct_expense_dates_extracted = [date['data'] for date in distinct_expense_dates_for_that_keeper]
-    list_for_chart = [['Date', 'EUR']]
+    list_for_chart = []
     for date in distinct_expense_dates_extracted:
         expenses_on_that_date = Expenses.objects.filter(data=date)
         sum_of_expenses_on_that_day = expenses_on_that_date.aggregate(Sum('suma'))
         sum_of_expenses_on_that_day_nicely_printed = sum_of_expenses_on_that_day['suma__sum']
         list_for_chart.append([date.strftime('%F'), float(sum_of_expenses_on_that_day_nicely_printed)])
+    if list_for_chart:
+        list_for_chart.insert(0, ['Date', 'EUR'])
+    print(list_for_chart)
     return list_for_chart
-
 
 def expenditure_by_keepers_expense_types_for_google_chart(keeper_id):
     all_expense_types_for_current_keeper = ExpenseTypes.objects.filter(keeper=keeper_id).order_by('tipas')
     expense_type_names = [expense_type.tipas for expense_type in all_expense_types_for_current_keeper]
-    list_for_chart = [['Expense type', 'Total Sum']]
+    list_for_chart = []
     for expense_type_name in expense_type_names:
         expenses_for_type = Expenses.objects.filter(tipas__tipas=expense_type_name, keeper=keeper_id)
         sum_of_expenses_for_type = expenses_for_type.aggregate(Sum('suma'))
         sum_of_expenses_for_type_nicely_printed = sum_of_expenses_for_type['suma__sum'] or 0.00
         list_for_chart.append([expense_type_name, float(sum_of_expenses_for_type_nicely_printed)])
+    if list_for_chart:
+        list_for_chart.insert(0, ['Expense type', 'Total Sum'])
+    print(list_for_chart)
     return list_for_chart
 
 def home(request):
