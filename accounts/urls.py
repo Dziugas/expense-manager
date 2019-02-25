c4be1484b5ce6102703aaaba72b9e7f19b0192b4
Dziugas <dziugas.tornau@gmail.com>
2019-02-25 17:27:27 +0200

    Users authorized to see only their own user page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('users/<int:user_id>/not_authorized/', views.user_not_authorized, name='user_not_authorized'),
     path('users/<int:user_id>/', views.viewUser, name='viewUser'),
-    path('in/', views.loggedIn, name='loggedIn')
+    path('in/', views.loggedIn, name='loggedIn'),
 ]
